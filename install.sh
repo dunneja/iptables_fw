99bb0e86a0d20a09a7c75fa06bf6693c4b11419b
James Dunne <james.dunne1@gmail.com>
2023-01-16 10:48:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,3 +26,24 @@ echo "Starting the systemd firewall.service"
 systemctl start firewall
 
 echo "Finished installation of IPTables Firewall"
+
+echo "-------------------------------------"
+echo "IPTables Logging Config Setup        "
+echo "-------------------------------------"
+
+echo "Making /var/log/iptables directory"
+
+if [ ! -d /var/log/iptables ]; then
+  mkdir -p /var/log/iptables;
+fi
+
+echo "Setting /var/log/iptables ownership"
+chown syslog:adm /var/log/iptables
+
+echo "Copying 10-iptables.conf to /etc/rsyslog.d/"
+cp 10-iptables.conf /etc/rsyslog.d
+
+echo "Restarting rsyslog.d Service"
+systemctl restart rsyslog.service
+
+echo "Finished installation of IPTables Logging"
